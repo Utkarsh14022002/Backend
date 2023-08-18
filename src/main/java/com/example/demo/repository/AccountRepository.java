bca52c911ecf465cbd9acbd68a90c4475e3270fa
mayuri9702 <mayuri.tambe@cumminscollege.in>
2023-08-18 13:58:18 +0530

    added custom method

--- src/main/java/com/example/demo/repository/AccountRepository.java
+++ src/main/java/com/example/demo/repository/AccountRepository.java
@@ -3,11 +3,15 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import com.example.demo.model.Account;
 import com.example.demo.model.Login;
 
 public interface AccountRepository extends JpaRepository<Account,Long> {
+	
+	@Query
+	public void createNewAccount(@RequestBody Account account);
 
 	
 }
