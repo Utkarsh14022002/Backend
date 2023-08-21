2dbe71729f5b395d12c4725fb7c9cc2e395bb9df
mayuri9702 <mayuri.tambe@cumminscollege.in>
2023-08-21 21:49:53 +0530

    payee controller changed

--- src/main/java/com/example/demo/rest/PayeeController.java
+++ src/main/java/com/example/demo/rest/PayeeController.java
@@ -4,6 +4,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,6 +19,7 @@ import com.example.demo.repository.PayeeRepository;
 
 @RestController
 @RequestMapping("/payees")
+@CrossOrigin(origins="*")
 public class PayeeController {
 	
 	@Autowired
