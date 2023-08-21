f30aa1fe7a3286d94b9c7c8f8d1346336e4bab72
mayuri9702 <mayuri.tambe@cumminscollege.in>
2023-08-21 16:12:32 +0530

    add payee code inserted

--- src/main/java/com/example/demo/rest/PayeeController.java
+++ src/main/java/com/example/demo/rest/PayeeController.java
@@ -1,12 +1,19 @@
 package com.example.demo.rest;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.model.Account;
+import com.example.demo.model.Login;
 import com.example.demo.model.Payee;
+import com.example.demo.repository.AccountRepository;
 import com.example.demo.repository.PayeeRepository;
 
 @RestController
@@ -15,12 +22,28 @@ public class PayeeController {
 	
 	@Autowired
 	private PayeeRepository payeeRepository;
+	@Autowired
+	private AccountRepository accountRepository;
 	
 	@PostMapping
 	public Payee createPayee(@RequestBody Payee payee)
 	{
 		payeeRepository.save(payee);
 		return payee;
+	}
+	
+	@PostMapping("/{accountNo}")
+	public ResponseEntity<String> addPayee(@PathVariable("accountNo") long accountNo,@RequestBody Payee payee){
+		Optional<Account> accOptional = accountRepository.findByAccountNo(accountNo);
+		if (accOptional.isPresent()) {
+			Account acc = accOptional.get();
+			payee.setAccountNoFromAccount(acc);
+			payeeRepository.save(payee);
+			return ResponseEntity.ok("New Payee added successfully");
+		}else {
+			return ResponseEntity.notFound().build();
+			
+		}
 	}
 
 }
